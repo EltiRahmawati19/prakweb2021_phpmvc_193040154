84375aa084559af5b03338121c61b70c9aab2a2c
EltiRahmawati19 <59133841+EltiRahmawati19@users.noreply.github.com>
2021-10-01 17:01:22 +0700

    Menggunakan bootstrap untuk mempercantik tampilan

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="8">
-    <title>Halaman About</title>
-</head>
-<body>
+
+<div class="container mt-4">
+  <img src="<?= BASEURL; ?>/img/193040154.jpg" alt="Elti Rahmawati" width="200" class="rounded-circle shadow">
  <h1>About me</h1>
     <p>Halo nama saya <?= $data['nama']; ?> , umur saya <?= $data['umur']; ?>
     tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
-</body>
-</html>
+
+  </div>
